bcd7d0e02ca279832bae7ddf66d9e32d2dd6d4ff
Nauw1010 <57137372+Nauw1010@users.noreply.github.com>
2020-09-21 20:49:26 +0800

    Revert "Pull from team repo"

--- src/main/java/seedu/address/model/journal/exceptions/DuplicateEntryException.java
+++
@@ -1,7 +0,0 @@
-package seedu.address.model.journal.exceptions;
-
-public class DuplicateEntryException extends RuntimeException {
-    public DuplicateEntryException() {
-        super("You can not add a duplicate entry");
-    }
-}
--- src/main/java/seedu/address/model/journal/exceptions/EntryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package seedu.address.model.journal.exceptions;
-
-public class EntryNotFoundException extends RuntimeException {
-    public EntryNotFoundException() {
-        super("Cannot find the entry");
-    }
-}
